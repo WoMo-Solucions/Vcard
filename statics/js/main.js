e68c73d2cffc36c6e46d8a99d4bf1098ee88a91a
Julian Ramirez <juliram81@gmail.com>
2025-08-09 23:48:26 -0500

    Delete statics/js/main.js

--- statics/js/main.js
+++
@@ -1,4 +0,0 @@
-// Aquí puedes agregar funcionalidades futuras si las deseas
-// Por ejemplo: copiar contacto, mostrar QR, animaciones, etc.
-
-console.log("Vcard WoMo Soluciónˢ cargada correctamente.");
